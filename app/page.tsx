151f0079df967076ae1ebcfdd2d1b3ff70181b34
giannkbr <akbarigi20@gmail.com>
2024-07-19 14:50:49 +0700

    refactor : modify desc

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">Built this for Programming test in - {siteConfig.name}</h1>
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">Built this for Programming test at - {siteConfig.name}</h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
           {siteConfig.description}
           <p> Tech stack used </p>
